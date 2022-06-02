89e7454f59bda9492edad059cdc97eb4742326a5
francodiaz7 <104851955+francodiaz7@users.noreply.github.com>
2022-06-02 16:48:29 -0300

    subida tarea

---
+++ main.js
@@ -0,0 +1,44 @@
+const pagos = [];
+const usuarios = [];
+const listado = document.getElementById("list-group");
+const cuadroResultado = document.getElementById("total");
+const usuario = document.getElementById("nombre");
+const pago = document.getElementById("pago");
+
+// alt +96 para crear backtips
+
+function enviar() {
+  agregarGastosAListas();
+  ultimoAPantalla();
+  mostrarPagoIndividualEnHTML();
+}
+
+function agregarGastosAListas() {
+  usuarios.push(usuario.value);
+  pagos.push(parseInt(pago.value));
+}
+
+function ultimoAPantalla() {
+  const li = document.createElement("li");
+  const text = document.createTextNode(`${usuario.value}: Pagó $${pago.value}`);
+  li.classList.add("list-group-item");
+  li.appendChild(text);
+  listado.appendChild(li);
+}
+
+function sumarValores(pagos) {
+  let suma = 0;
+  for (let pago of pagos) {
+    suma += parseInt(pago);
+  }
+  return suma;
+}
+
+function mostrarPagoIndividualEnHTML() {
+  const total = sumarValores(pagos);
+  const aporteIndividual = total / usuarios.length;
+  cuadroResultado.innerText = `Total: $${total}
+                                A cada uno le toca aportar: $${aporteIndividual.toFixed(
+                                  2
+                                )}`;
+}
